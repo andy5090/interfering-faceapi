e7b7f9a9a2af7fe54adc971a52b9bc14fc2f9152
Andy Lee <andy5090@naver.com>
2019-11-14 18:02:19 +0900

    first camera client complete

---
+++ index.js
@@ -0,0 +1,27 @@
+const gotDevices = deviceInfos => {
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    if (deviceInfo.kind == "videoinput") {
+      const option = document.createElement("option");
+      option.value = `${deviceInfo.deviceId}`;
+      option.innerHTML = `${i} ${deviceInfo.label}`;
+      select.appendChild(option);
+    }
+  }
+};
+
+const camSelected = () => {
+  console.log(select.value, "selected");
+  localStorage.setItem("mainCam", select.value);
+  location.href = "http://localhost:5000/start.html";
+};
+
+const select = document.createElement("select");
+select.addEventListener("change", camSelected);
+document.body.appendChild(select);
+
+const blankOption = document.createElement("option");
+blankOption.innerHTML = "-- select camera --";
+select.appendChild(blankOption);
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices);
